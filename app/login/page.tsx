ccb4e7da965f355a8a676cef94a4717cde18c2f6
samousavizade <samousavizade@gmail.com>
2024-08-22 17:28:07 +0330

    add

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import SignOut from '@/components/SignOut';
+import SignIn from '@/components/SignIn';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <SignIn />
+      <SignOut />
+    </div>
+  );
+}
